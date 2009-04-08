c90e3f34f8acf4cdc033e528295451398c45c270
Raphael Kubo da Costa <kubito@gmail.com>
2009-04-08 16:19:54 -0300

    Finish doxyfication in futex.h

--- futex.h
+++ futex.h
@@ -10,6 +10,9 @@
 /**
  * Wrapper around SYS_futex with FUTEX_WAIT.
  *
+ * @param addr The address of the futex.
+ * @param val1 The value to compare the futex to.
+ *
  * @retval -1 Futex hasn't blocked.
  * @retval 0  Futex has blocked.
  */
@@ -18,7 +21,9 @@ int futex_wait(void *addr, int val1);
 /**
  * Wrapper around SYS_futex with FUTEX_WAKE.
  *
- * @param  n The number of threads to awake (or INT_MAX to wake all)
+ * @param addr The address of the futex.
+ * @param n    The number of threads to awake (or INT_MAX to wake all)
+ *
  * @return The number of threads which were awakened.
  */
 int futex_wake(void *addr, int n);
